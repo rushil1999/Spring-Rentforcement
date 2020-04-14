d36fb72f5044466a5df8d13edde89755c4067449
rushil1999 <shahrushil1999@gmail.com>
2020-04-14 16:13:46 +0530

    Reverting to Backend and Rest API

--- src/main/java/com/rental/user/UserController.java
+++ src/main/java/com/rental/user/UserController.java
@@ -1,16 +1,15 @@
 package com.rental.user;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.rental.handler.CustomException;
 
 @Controller
@@ -22,22 +21,42 @@ public class UserController {
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/addUser")
 	@ResponseBody
-	public String signUp(@RequestBody User user) throws CustomException{
-		String str = null;
+	public ResponseEntity<Boolean> signUp(@RequestBody User user) throws CustomException{
 		if(userServ.addUser(user)) {
-			str = "Sign Up Successfull";
-			return str;
+			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
 			
 		}
 		else {
-			return "Failure";
+			return new ResponseEntity<Boolean>(false, HttpStatus.INTERNAL_SERVER_ERROR);
 		}	
 		
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/signUp")
-	public String display() {
-		return "SignUp";
+	@RequestMapping(method = RequestMethod.GET, value = "/users")
+	@ResponseBody
+	public ResponseEntity<ArrayList<User>> getUserlist(){
+		ArrayList<User> list = new ArrayList<User>();
+		list = userServ.getUserList();
+		return new ResponseEntity<ArrayList<User>>(list, HttpStatus.OK);
 	}
-
 }
+
+
+
+
+
+
+
+
+
+//@RequestMapping(method = RequestMethod.GET, value = "/signUp1")
+//public String display1() {
+//	System.out.println("Rushil Shah");
+//	return "signUp";
+//}
+//
+//@RequestMapping(method = RequestMethod.GET, value = "/signUp2")
+//public String display2() {
+//	System.out.println("Rushil Shah");
+//	return "loginSignUp";
+//}
